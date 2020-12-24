0e6bea3400f5db5a142fe7be20202528648d3478
johannes wasmer <johannes.wasmer@gmail.com>
2020-12-24 15:31:19 +0100

    sisclab restructured D1,D2 notebook supervisor instructions, added helpers example

--- aiida_jutools/sisc_lab/helpers.py
+++ aiida_jutools/sisc_lab/helpers.py
@@ -1,4 +1,11 @@
-
-
 # In here you can implement all functions you need within the notebooks
 # you can import them via `from .helpers.import <function_namw>` 
+
+
+def print_bold(text: str):
+    """Print text in bold.
+
+    :param text: text to print in bold.
+    """
+    bold_text = f"\033[1m{text}\033[1m"
+    print(bold_text)
