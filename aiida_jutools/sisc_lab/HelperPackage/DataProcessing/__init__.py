b07c3ec5a463fe76a2e938011a4db0bc2e89a52f
Zhipeng Tan <zhipeng.tan@rwth-aachen.de>
2021-01-08 18:00:07 +0100

    fix the structure analysis plots

--- aiida_jutools/sisc_lab/HelperPackage/DataProcessing/__init__.py
+++ aiida_jutools/sisc_lab/HelperPackage/DataProcessing/__init__.py
@@ -1,8 +1,13 @@
 # __init__.py
-# the code to initialization
+# the code to initialization and add all modules to sys path
 import sys
 from pathlib import Path
 import os
+import aiida
+
+
+aiida.load_profile()
+
 
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -12,11 +17,10 @@ def add_to_sys_path(path:Path):
 
 add_to_sys_path(path)
 
-print(path)
-
-#import DataProcessing.DataVisu
 
 if __name__ == 'main':
     print(path,'is added to system path')
 else:
-    print('__init__.py Initialization successful')
+    print(path+' loaded successfully')
+    print('DataProcess Package Initialization successfully')
+    print('Aiida file loaded successfully')
