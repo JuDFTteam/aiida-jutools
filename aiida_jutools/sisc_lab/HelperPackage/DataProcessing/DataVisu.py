dd1d0cce727c9a5bcf9a251e4a32939a86c2f623
Zhipeng Tan <zhipeng.tan@rwth-aachen.de>
2021-01-19 23:52:48 +0100

    add 1.f function to helper package

--- aiida_jutools/sisc_lab/HelperPackage/DataProcessing/DataVisu.py
+++ aiida_jutools/sisc_lab/HelperPackage/DataProcessing/DataVisu.py
@@ -9,8 +9,8 @@ def add_to_sys_path(path:Path):
 from itertools import groupby
 import re
 
-## class StrucData to analyse the 
-class StrucDataForm:
+## class StrucData to analyse the elements
+class StrucDataHelper:
     'To get the StructureData elements from the formula'
     def __init__(self,formula):
         self.Formula = formula
@@ -47,4 +47,31 @@ class StrucDataForm:
             mystr[group[0]] = group[1]
         return mystr
     
+
     
+### List group nodes and their number
+class GroupDataHelper:
+    def __init__(self,group : list):
+        '''
+        :param group: the group data list for initialization
+        '''
+        self.Group = group
+        
+    def ListGroup(self,exclude: list=[]):
+        """ return the group names and nodes they contain
+        :param exclude: the list of data which we don't want to show
+        """
+        print('{:<52}{:6}'.format('Group names:','sizes:'))
+        for a in self.Group:
+            flag=0
+            type = a[0].type_string
+            for ex in exclude:
+                if ex in type:
+                    flag=1
+            if(flag):
+                continue     
+            else:
+                ## the line below contains all the properties
+                ##print(a[0].label,' ',a[0].user,' ',a[0].type_string,' ',a[0].description)
+
+                print('{:<50}|{:5}'.format(a[0].label,len(a[0].nodes)))
