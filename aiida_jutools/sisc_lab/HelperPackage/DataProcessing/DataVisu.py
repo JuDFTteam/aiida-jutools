e141b75846b41162d516830f3a1f4e3891413c93
Zhipeng Tan <zhipeng.tan@rwth-aachen.de>
2021-01-20 17:02:00 +0100

    add serialization to f and g

--- aiida_jutools/sisc_lab/HelperPackage/DataProcessing/DataVisu.py
+++ aiida_jutools/sisc_lab/HelperPackage/DataProcessing/DataVisu.py
@@ -8,9 +8,13 @@ def add_to_sys_path(path:Path):
 
 from itertools import groupby
 import re
+from aiida.orm import QueryBuilder,StructureData
+import numpy as np
 
-## class StrucData to analyse the elements
-class StrucDataHelper:
+
+####### function to analyse structure data elements number and nodes number for 1.g ###############################
+## class StrucData to analyse the elements 
+class StrucFormulaHelper:
     'To get the StructureData elements from the formula'
     def __init__(self,formula):
         self.Formula = formula
@@ -46,10 +50,123 @@ class StrucDataHelper:
             group = self.seperate_String_Num(s)
             mystr[group[0]] = group[1]
         return mystr
-    
+
+
+####### function to analyse structure data  ###############################    
+import numpy as np
+from bokeh.io import output_file, show
+from bokeh.models import ColumnDataSource,HoverTool
+from bokeh.plotting import figure
+from bokeh.io import output_notebook
+from bokeh.palettes import inferno
+import pandas as pd
+
+def AtomsNumNodes(StructDatas):
+    '''
+    This function return atom numbers and how many Nodes have this atom number;
+    Out put can be passed to ShowFormula directly or de-/serialization
+    '''
+    #qb = QueryBuilder()
+    #qb.append(StructureData)
+    #StructDatas = qb.all()
+    Newdict = {}
+
+    for data, in StructDatas:
+        CompositionDict = data.get_composition()
+        NumAtom = int(np.sum(list(CompositionDict.values())))
+        if NumAtom in Newdict.keys():
+            Newdict[NumAtom].append('['+data.uuid[:8])
+            Newdict[NumAtom].append(data.get_formula()+']')
+        else:
+            Newdict[NumAtom] = ['['+data.uuid[:8],data.get_formula()+']']
+            
+    return Newdict
 
     
-### List group nodes and their number
+def AnalyseStructureElements(InputData):
+    '''
+    This function count the Elements and number of StructureNode
+    Output can be passed to ShowElements
+    '''
+    #### return the pd.DataFrame including elements and number of each element
+
+    StrucList = []
+    #qb = QueryBuilder()
+    #qb.append(StructureData)
+    #print('number of StructureData Nodes:',qb.count())
+    #qb.count()
+
+    for struc, in InputData:
+        form = struc.get_formula()
+        struct = StrucFormulaHelper(form)
+        StrucList = StrucList+ [struct.FormAnalyse()]
+    return pd.DataFrame(StrucList).fillna(0)
+
+def ShowElements(Data):
+    #### visualize the Elements and number of them
+    output_file("ShowingElements.html")
+    #data = NumStructureNode()
+    data = Data
+    elements = list(data.columns)
+    counts = list(data.astype(bool).sum(axis=0))
+    # zip sort
+    counts,elements = zip(*sorted(zip(counts,elements)))
+    
+    #print(counts)
+    #print(elements)
+    
+    source = ColumnDataSource(data=dict(elements=elements, counts=counts,color=inferno(len(elements))))
+    
+    TOOLTIPS = [
+    ("element", "@elements"),
+    ("(x,y)", "($x, $y)"),
+    ("Number of Structures containing this element", "@counts"),
+    ]
+
+    p = figure( y_range=elements,x_range=(0,np.max(counts)), plot_width=800, plot_height=800, title="Elements Counts",tools = [HoverTool(mode='hline')], tooltips=TOOLTIPS)
+    #print('step figure done')
+    p.hbar(y="elements", right="counts", height=0.5, left=0, color='color',  source=source)
+    #print('step hbar done')
+    
+    output_notebook()
+    p.xgrid.grid_line_color = None
+    #p.legend = False
+    show(p)
+    
+    
+def ShowFormula(Data):
+    ## Show the formula and id
+    
+
+    output_file("ShowingFormula.html")
+    data = Data
+    elements = list(data.keys())
+    counts = list(len(data[key])/2 for key in data.keys())
+    formulas = list(data[key][:10] for key in data.keys())
+
+    length = len(elements)
+    source = ColumnDataSource(data=dict(elements=elements, counts=counts,formulas=formulas,color=inferno(length)))
+    
+    TOOLTIPS = [
+    ("Number of Atoms", "@elements"),
+    ("(x,y)", "($x, $y)"),
+    ("Number of Nodes", "@counts"),
+    ("Id and formula(first 5 nodes of all)", "@formulas"),
+    ]
+
+    p = figure(x_range=(0,np.max(counts)+20),y_range=(0,np.max(elements)), plot_width=800, plot_height=800, title="Atoms Count",tools = [HoverTool(mode='hline')], tooltips=TOOLTIPS)
+    #print('step figure done')
+    p.hbar(y="elements", right="counts", height=0.5, left=0, color='color',  source=source)
+    #print('step hbar done')
+    
+    output_notebook()
+    p.xgrid.grid_line_color = None
+    #p.legend = False
+    show(p)
+
+#################################################### end #####################################################
+
+####################################### List group nodes and their number for 1.f#################################
 class GroupDataHelper:
     def __init__(self,group : list):
         '''
@@ -62,9 +179,10 @@ class GroupDataHelper:
         :param exclude: the list of data which we don't want to show
         """
         print('{:<52}{:6}'.format('Group names:','sizes:'))
-        for a in self.Group:
+        for index, row in self.Group.iterrows():
+            
             flag=0
-            type = a[0].type_string
+            type = row['type_string']
             for ex in exclude:
                 if ex in type:
                     flag=1
@@ -74,4 +192,4 @@ class GroupDataHelper:
                 ## the line below contains all the properties
                 ##print(a[0].label,' ',a[0].user,' ',a[0].type_string,' ',a[0].description)
 
-                print('{:<50}|{:5}'.format(a[0].label,len(a[0].nodes)))
+                print('{:<50}|{:5}'.format(row['Group_Name'],row['Node']))
--- aiida_jutools/sisc_lab/HelperPackage/Serialization/Serialization.py
+++ aiida_jutools/sisc_lab/HelperPackage/Serialization/Serialization.py
@@ -1,10 +1,75 @@
 ### Class serialized is used to save data to file
 import pandas as pd
+import HelperPackage.DataProcessing.DataVisu as DV
+import json
 
 class Serializer:
     def __init__(self,data):
         self.data = data
-    def to_file(self,filepath):
-        Data = pd.DataFrame(self.data)
-        Data.to_json(filepath,orient='records')
+    def to_file(self,filepath,Node_type = None):
+        '''
+        This function serialize different data according to the Node_Type keyword
+        So be careful to specify the type when doing analyse
+        '''
+        ######## None type
+        if(Node_type == None):
+            return 0
+        #### Group type 
+        elif(Node_type == 'Group'):
+            DataList = []
+            Data = {}
+            Columns = ['User','Group_Name','Node','type_string']
+            for column in Columns:
+                Data[column] = []
+            for g, in self.data:
+                DataList = DataList + [[g.user.get_short_name(),g.label,len(g.nodes),g.type_string]]
+
+                '''
+                Data['User'] = Data['User'] + [g[0].user]
+                Data['Grouplabel'] = Data['Grouplabel'] + [g[0].label]
+                Data['Node'] = Data['Node'] + [len(g[0].nodes)]
+
+                Data['User'] = [g.user]
+                Data['Grouplabel'] = [g.label]
+                Data['Node'] = [len(g.nodes)]
+
+                DataList = DataList + [Data]
+
+                '''
+
+            #print(DataList)
+            Data = pd.DataFrame(DataList,columns = Columns)
+            ## the aiida object will cause error here, so ensure every value in the list is no object
+            Data.to_json(filepath,orient='records')
+            return 0
+        ### structureFormula type
+        elif(Node_type == 'StructureFormula'):
+            dic = DV.AtomsNumNodes(self.data)
+            with open(filepath, 'w') as fp:
+                json.dump(dic,fp)
+            return 0
         
+        ###### structure Element type
+        elif(Node_type == 'StructureElement'):
+            
+            Data = DV.AnalyseStructureElements(self.data)
+            Data.to_json(filepath)
+        
+    
+def deserialize_from_file(filepath,Node_type = None):
+    if(Node_type == None):
+        return 0
+    elif(Node_type == 'Group'):
+        
+        return 0
+    elif(Node_type == 'StructureFormula'):
+        with open(filepath, 'r') as f:
+            data2 = json.load(f)
+            
+        ############ since the key here is saved to a string, but we want a int, so do the following to transfer
+        Newdata = {}
+        for key in data2:
+            #print(int(key))
+            Newdata[int(key)] = data2[key]
+        return Newdata
+    
