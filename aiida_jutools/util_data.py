f5ca3bd4513f01b51153959c0c33843c6074c493
johannes wasmer <johannes.wasmer@gmail.com>
2021-02-28 10:44:30 +0100

    added rescale_structures()

--- aiida_jutools/util_data.py
+++ aiida_jutools/util_data.py
@@ -14,11 +14,6 @@
 
 from pathlib import Path
 
-from aiida.orm import CifData
-from aiida.orm import Dict, Group
-from aiida.orm import StructureData, QueryBuilder
-from aiida.tools.groups import GroupPath
-
 
 # @calcfunction
 # def cif2structure(cif:CifData, converter='pymatgen', store:bool=True, conversion_settings:Dict=Dict({})) -> StructureData:
@@ -33,6 +28,8 @@ def query_elemental_structure(symbol: str, group=None) -> list:
     :type group:
     :return: list of results
     """
+    from aiida.orm import QueryBuilder, Group, StructureData
+
     qb = QueryBuilder()
     if group:
         qb.append(Group, filters={'label': group.label}, tag='group')
@@ -84,6 +81,8 @@ class CifImporter:
         :return: unstored CifData node if file ends with ".cif", case-insensitive, else None.
         :rtype: CifData or None
         """
+        from aiida.orm import CifData
+
         if not cif_file.exists():
             raise FileNotFoundError(f"File {cif_file} does not exist.")
 
@@ -108,6 +107,8 @@ class CifImporter:
         :param cif_group_label: if supplied, add CifData nodes to this group. Create if not exist.
         :return: list of unstored CifData nodes, or group with stored CifData nodes, if supplied.
         """
+        from aiida.tools.groups import GroupPath
+
         cifs = []
         for cif_file in cif_files_path.iterdir():
             cif = self.from_file(cif_file=cif_file, silent=True)
@@ -129,7 +130,7 @@ class CifImporter:
 
     def load_or_convert(self, cifgroup_label: str, structure_group_label: str, as_subgroup: bool = True,
                         structure_group_description: str = None, load_over_create: bool = True,
-                        conversion_settings: Dict = None, dry_run: bool = True, silent: bool = False):
+                        conversion_settings=None, dry_run: bool = True, silent: bool = False):
         """Load or create group of StructureData nodes from group of CifData nodes.
 
         :param cifgroup_label: group label of CifData group.
@@ -138,6 +139,7 @@ class CifImporter:
         :param structure_group_description: description for converted group. ignored if already exist and stored.
         :param load_over_create: if converted group exists and has structure nodes: load nodes instead of create (convert).
         :param conversion_settings: settings arguments supplied to the respective CifData.to_OtherFormat() method.
+        :type conversion_settings: Dict
         :param dry_run: default True: perform a dry run and print what the method *would* do.
         :param silent: default True: if not dry_run do not print info messages.
         :return: converted group
@@ -152,6 +154,9 @@ class CifImporter:
 
         Note: newly created converted data nodes label and description may be changed, even after having been already stored.
         """
+        from aiida.orm import Group, CifData, StructureData
+        from aiida.tools.groups import GroupPath
+
         # DEVNOTE:
         # group.add_nodes([node]) works only if both group and node are stored. keep in mind when changing
         # the code logic order.
@@ -216,6 +221,8 @@ class CifImporter:
             self.struc_group = struc_group
 
         def _load_or_create_conversion_settings() -> None:
+            from aiida.orm import Dict
+
             def _try_load_conversion_settings(struc_group):
                 if not struc_group:
                     return None
@@ -326,7 +333,6 @@ class CifImporter:
                       f"{len(structure_nodes)} {StructureData.__name__} nodes. It is recommended to keep a one-to-one "
                       f"node-to-node conversion relation between such conversion groups to corectly reflect provenance.")
 
-
         cif_nodes = _get_cif_nodes()
         if cif_nodes:
             struc_grouppath = _determine_structure_group_path(structure_group_label=structure_group_label)
@@ -337,3 +343,109 @@ class CifImporter:
             _post_conversion_check(cif_nodes=cif_nodes, struc_grouppath=struc_grouppath, structure_nodes=struc_nodes)
 
         return self.struc_group
+
+
+def load_or_rescale_structures(input_structure_group, output_structure_group_label: str, scale_factor,
+                               dry_run:bool = True, silent: bool = False):
+    """Rescale a group of structures and put them in a new or existing group.
+
+    Only input structures which do not already have a rescaled output structure in the output structure group
+    will be rescaled.
+
+    :param input_structure_group: group with StructureData nodes to rescale. Ignores other nodes in the group.
+    :type input_structure_group: Group
+    :param output_structure_group_label: name of group for rescaled structures. Create if not exist.
+    :param scale_factor: scale factor with which to scale the lattice constant of the input structure
+    :type scale_factor: Float
+    :param dry_run: default True: perform a dry run and print what the method *would* do.
+    :param silent: True: do not print info messages
+    :return: output group of rescaled structures
+    :rtype: Group
+    """
+    from aiida.tools.groups import GroupPath
+    from aiida.orm import Group, StructureData, Float
+    from aiida_jutools.process_functions.rescale_structure import rescale_structure
+
+    assert isinstance(scale_factor, Float)
+
+    would_or_will = "would" if dry_run else "will"
+
+    out_structure_grouppath = GroupPath(path=output_structure_group_label)
+    out_structure_group, created = out_structure_grouppath.get_or_create_group()
+
+    inp_structures = {node.uuid: node for node in input_structure_group.nodes if isinstance(node, StructureData)}
+    already_rescaled = {}
+
+    if dry_run or not silent:
+        print(40 * '-')
+        print(f"Task: Rescale {len(inp_structures.keys())} {StructureData.__name__} nodes in group "
+              f"{input_structure_group.label}' with scale factor {scale_factor.value}.\nPerform dry run: {dry_run}.")
+
+    # try load structures
+    out_structures_existing = [node for node in out_structure_group.nodes if isinstance(node, StructureData)]
+    # now pop out the input nodes which already have been rescaled
+    for out_struc in out_structures_existing:
+        inps_from_out = query_modified_input_structure(modified_structure=out_struc, invariant_kinds=True)
+        if inps_from_out:
+            uuids = [inp.uuid for inp in inps_from_out]
+            for uuid in uuids:
+                if uuid in inp_structures:
+                    already_rescaled[uuid] = inp_structures.pop(uuid)
+
+    # now rescale the remaining ones
+    if dry_run or not silent:
+        print(
+            f"I {would_or_will} rescale {len(inp_structures.keys())} {StructureData.__name__} nodes from the input group. "
+            f" I would add the new nodes to output group '{output_structure_group_label}'. "
+            f" {len(already_rescaled.keys())} {StructureData.__name__} of the input nodes already have been rescaled and added "
+            f"to this output target previously.")
+    if not dry_run:
+        for inp_structure in inp_structures.values():
+            out_structure = rescale_structure(input_structure=inp_structure, scale_factor=scale_factor)
+            out_structure_group.add_nodes([out_structure])
+    if not dry_run and not silent:
+        print(
+            f"Created {len(inp_structures.keys())} {StructureData.__name__} nodes and added them to group "
+            f"'{output_structure_group_label}'.")
+    return out_structure_group
+
+
+def query_modified_input_structure(modified_structure, invariant_kinds: bool = False) -> list:
+    """Given a structure modified via a CalcFunction, query its input structure(s).
+
+    :param modified_structure: structure modified via a single CalcFunction
+    :type modified_structure: StructureData
+    :param invariant_kinds: to make query more precise., assume that the 'kinds' attribute has not been modified.
+    :return: list of input structures, if any.
+    """
+    from aiida.orm import QueryBuilder, CalcFunctionNode, StructureData
+
+    def _filter_from_attribute(attribute: list) -> dict:
+        """Unpack a complex attribute into an 'and'-query filter.
+
+        :param attribute: attribute of a node. Assumes list of dicts of simple types or list thereof.
+        :return: a query filter for nodes with that attribute and those values
+        """
+        filters = {'and': []}
+        for i, kind in enumerate(attribute):
+            for key, value in kind.items():
+                if not isinstance(value, list):
+                    filters['and'].append({f"attributes.kinds.{i}.{key}": attribute[i][key]})
+                else:
+                    for j, val in enumerate(value):
+                        filters['and'].append({f"attributes.kinds.{i}.{key}.{j}": attribute[i][key][j]})
+        return filters
+
+    if not invariant_kinds:
+        input_structure_filters = {}
+    else:
+        output_kinds = modified_structure.attributes['kinds']
+        input_structure_filters = _filter_from_attribute(attribute=output_kinds)
+
+    qb = QueryBuilder()
+    # qb.append(Group, filters={'label': output_structure_group.label}, tag='group')
+    # qb.append(StructureData, with_group='group', filters={"uuid" : modified_structure.uuid}, tag='out_struc')
+    qb.append(StructureData, filters={"uuid": modified_structure.uuid}, tag='out_struc')
+    qb.append(CalcFunctionNode, with_outgoing='out_struc', tag='calc_fun')
+    qb.append(StructureData, with_outgoing='calc_fun', filters=input_structure_filters)
+    return qb.all(flat=True)
