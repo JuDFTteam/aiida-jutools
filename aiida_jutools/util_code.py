9f05b5c704fd621f9d3d499e385f0a41694bd1aa
johannes wasmer <johannes.wasmer@gmail.com>
2021-04-02 14:07:03 +0200

    renamed get_code_for_computer_queue -> get_code, and made all arguments optional (still works)

--- aiida_jutools/util_code.py
+++ aiida_jutools/util_code.py
@@ -13,27 +13,30 @@
 """Tools for working with aiida Code nodes."""
 
 
-def get_code_for_computer_queue(computer_name: str, queue_name: str, code_name_pattern: str):
-    """Get the appropriate code for computer based on given computer queue (slurm: partition) and code name.
+def get_code(computer_name_pattern: str = "", code_name_pattern: str = "", queue_name: str = ""):
+    """Find a matching code. If queue_name given, choose code with appropriate architecture.
 
-    This will choose the appropriate code under the assumption that different queues (partitions) of the respective
-    computer require the code to be compiled with different architecture. It is assumed that the code labels
-    either have a substring which specifies the which specifies the computer queue name, or a substring
-    which specifies the architecture.
+    All arguments are optional. defaults (empty strings), function will query all codes and choose first found.
+    Just try it out with different argument combinations to get a feel for the behavior.
+
+    If queue_name given, and applicable for this computer, this will choose the appropriate code under the assumption
+    that different queues (partitions) of the respective computer require the code to be compiled with different
+    architecture. For this to work, it is assumed that the code labels either have a substring which specifies the
+    which specifies the computer queue name, or a substring which specifies the architecture.
 
     All performed substring matches are case-insensitive.
 
-    Implementations available for these computers:
+    Queue_name <-> architecture code matching available for these computers:
     - 'iffslurm': FZJ PGI-1 iffslurm cluster.
 
-    Implementations available for these architectures:
+    Queue_name <-> architecture code matching available for these architectures:
     - 'intel'
     - 'AMD'
 
-    :param computer_name: exact computer label
-    :param queue_name: name of the computer queue (slurm: partition)
-    :param code_name_pattern: code label substring identifying a subset of codes (eg a 'kkrimp', 'fleur', ...)
-    :return: the appropriate code for the specified queue
+    :param computer_name_pattern: substring matching some computer label(s)
+    :param queue_name: exact name of the computer queue (slurm: partition)
+    :param code_name_pattern: substring matching some code label(s)
+    :return: closest matching code. if found several, return first warn, but print all matches
     :rtype: Code
     """
     from aiida_jutools import util_computer
@@ -68,10 +71,10 @@ def get_code_for_computer_queue(computer_name: str, queue_name: str, code_name_p
         :return: tuple (selected codestring, error_msg). error_msg None if success, else None.
         """
         msg_middle_queue = "" if not queue_name else f", computer queue '{queue_name}'"
-        msg_middle = f"for specified computer '{computer_name}'{msg_middle_queue}, code " \
+        msg_middle = f"for specified computer '{computer_name_pattern}'{msg_middle_queue}, code " \
                      f"name pattern '{code_name_pattern}'{msg_suffix}."
 
-        warning_msg = f"WARNING: '{get_code_for_computer_queue.__name__}()': Ambiguous codestrings result " \
+        warning_msg = f"WARNING: '{get_code.__name__}()': Ambiguous codestrings result " \
                       f"{filtered_codestrings} while determining appropriate code {msg_middle} Will choose first " \
                       f"one. Resolve ambiguity by more precise code name pattern."
         all_codestrings = [f"{code.label}@{code.computer.label}" for code in Code.objects.all()]
@@ -90,20 +93,21 @@ def get_code_for_computer_queue(computer_name: str, queue_name: str, code_name_p
 
         return codestring, error_msg
 
-    computers = util_computer.get_computers(computer_name)
+    computers = util_computer.get_computers(computer_name_pattern)
     if not computers:
         from aiida.common.exceptions import NotExistent
-        raise NotExistent(f"No computer '{computer_name}' found.")
+        raise NotExistent(f"No computer '{computer_name_pattern}' found.")
     else:
         computer = computers[0]
         if len(computers) > 1:
-            print(f"WARNING: For computer name {computer_name}, found several computers {[c.label for c in computers]}. "
-                  f"Will choose first one.")
+            print(
+                f"WARNING: For computer name {computer_name_pattern}, found several computers {[c.label for c in computers]}. "
+                f"Will choose first one.")
 
     # get cs = codestrings needed for Code.get_from_string(), filter to desired codes on desired computers
     cs_by_computer_code = [f"{code.label}@{code.computer.label}" for code in Code.objects.all() if
-                    computer_name.lower() in code.computer.label.lower()
-                    and code_name_pattern.lower() in code.label.lower()]
+                           computer_name_pattern.lower() in code.computer.label.lower()
+                           and code_name_pattern.lower() in code.label.lower()]
 
     if not queue_name:
         # Case A): if no queue_name is supplied, only determine by computer_name and code_name_pattern
@@ -133,15 +137,15 @@ def get_code_for_computer_queue(computer_name: str, queue_name: str, code_name_p
             # computer labels in there is the closest match to computer_name.
             all_computer_labels = list(computer_queue_architectures.keys())
             matching_computer_labels = [label for label in all_computer_labels
-                                        if computer_name.lower() in label.lower()]
+                                        if computer_name_pattern.lower() in label.lower()]
             if not matching_computer_labels:
-                raise KeyError(f"For computer '{computer_name}', I have no hardcoded knowledge about "
+                raise KeyError(f"For computer '{computer_name_pattern}', I have no hardcoded knowledge about "
                                f"queue architectures, only for computers {all_computer_labels}.")
             else:
                 matching_computer_label = matching_computer_labels[0]
                 computer_queue_architectures = computer_queue_architectures[matching_computer_label]
                 if len(matching_computer_labels) > 1:
-                    print(f"WARNING: '{get_code_for_computer_queue.__name__}()': For computer {computer_name}, "
+                    print(f"WARNING: '{get_code.__name__}()': For computer {computer_name_pattern}, "
                           f"found more than one harcoded queue-architecture entry: {matching_computer_labels}. "
                           f"Will choose first one. If this is a problem, contact developer.")
 
@@ -149,7 +153,7 @@ def get_code_for_computer_queue(computer_name: str, queue_name: str, code_name_p
             queues_queried = util_computer.get_queues(computer=computer, gpu=None, with_node_count=False)
             if not set(queues_harcoded) == set(queues_queried):
                 raise ValueError(
-                    f"Computer '{computer_name}' hardcoded queues {queues_harcoded} do not "
+                    f"Computer '{computer_name_pattern}' hardcoded queues {queues_harcoded} do not "
                     f"correspond anymore to queried queues {queues_queried}. Update code.")
 
             # now find the appropriate code for the given queue
@@ -161,7 +165,7 @@ def get_code_for_computer_queue(computer_name: str, queue_name: str, code_name_p
                 # that user has specified non-existant queue
                 import inspect
                 module_name = inspect.getmodulename(inspect.getfile(util_computer.get_queues))
-                raise KeyError(f"Computer '{computer_name}' has no queue '{queue_name}'. Use "
+                raise KeyError(f"Computer '{computer_name_pattern}' has no queue '{queue_name}'. Use "
                                f"'{module_name}.{util_computer.get_queues.__name__}()' to get list of queues.")
 
             cs_by_computer_code_arch = [cs for cs in cs_by_computer_code if architecture in cs]
