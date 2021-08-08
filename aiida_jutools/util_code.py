3fc7de1e7de0684e23055d8e35c7b85193b141f9
johannes wasmer <johannes.wasmer@gmail.com>
2021-08-08 18:52:36 +0200

    util_code: refactor: imports, typing, simplify code

--- aiida_jutools/util_code.py
+++ aiida_jutools/util_code.py
@@ -12,8 +12,18 @@
 ###############################################################################
 """Tools for working with aiida Code nodes."""
 
+import inspect as _inspect
+import typing as _typing
 
-def get_code(computer_name_pattern: str = "", code_name_pattern: str = "", queue_name: str = ""):
+import aiida as _aiida
+import aiida.orm as _orm
+
+from aiida_jutools import util_computer as _jutools_computer
+
+
+def get_code(computer_name_pattern: str = "",
+             code_name_pattern: str = "",
+             queue_name: str = "") -> _orm.Code:
     """Find a matching code. If queue_name given, choose code with appropriate architecture.
 
     All arguments are optional. defaults (empty strings), function will query all codes and choose first found.
@@ -36,13 +46,10 @@ def get_code(computer_name_pattern: str = "", code_name_pattern: str = "", queue
     :param computer_name_pattern: substring matching some computer label(s)
     :param queue_name: exact name of the computer queue (slurm: partition)
     :param code_name_pattern: substring matching some code label(s)
-    :return: closest matching code. if found several, return first warn, but print all matches
-    :rtype: Code
+    :return: closest matching code. if found several, return first, but print all matches
     """
-    from aiida_jutools import util_computer
-    from aiida.orm import Code
 
-    def _hardcoded_queue_architectures() -> dict:
+    def _hardcoded_queue_architectures() -> _typing.Dict[str, _typing.Dict[str, str]]:
         """hardcoded knowledge about architecture of computer queues (slurm: partitions).
         :return: dict of {computer_name : {queue name : architecture name} }
         """
@@ -51,7 +58,7 @@ def get_code(computer_name_pattern: str = "", code_name_pattern: str = "", queue
             'intel': ['oscar', 'th1', 'th2-gpu', 'viti']
         }
 
-        def _invert(dict_architecture_queues):
+        def _invert(dict_architecture_queues: _typing.Dict[str, _typing.List[str]]):
             """{architecture : [queue names]} --> {queue_name : architecture}"""
             dict_queue_architecture = {}
             for arch, queues in dict_architecture_queues.items():
@@ -65,8 +72,9 @@ def get_code(computer_name_pattern: str = "", code_name_pattern: str = "", queue
         }
         return queue_architectures
 
-    def _select_codestring_from_filtered(codestrings_by_computer_code: list, filtered_codestrings: list,
-                                         msg_suffix: str = "") -> tuple:
+    def _select_codestring_from_filtered(codestrings_by_computer_code: _typing.List[str],
+                                         filtered_codestrings: _typing.List[str],
+                                         msg_suffix: str = "") -> _typing.Tuple[str, str]:
         """Selects first codestring from filtered if more than one, prints warning/error messages.
         :return: tuple (selected codestring, error_msg). error_msg None if success, else None.
         """
@@ -77,11 +85,11 @@ def get_code(computer_name_pattern: str = "", code_name_pattern: str = "", queue
         warning_msg = f"WARNING: '{get_code.__name__}()': Ambiguous codestrings result " \
                       f"{filtered_codestrings} while determining appropriate code {msg_middle} Will choose first " \
                       f"one. Resolve ambiguity by more precise code name pattern."
-        all_codestrings = [f"{code.label}@{code.computer.label}" for code in Code.objects.all()]
+        all_codestrings = [f"{code.label}@{code.computer.label}" for code in _orm.Code.objects.all()]
         error_msg = f"Could not determine appropriate code " \
                     f"{msg_middle} No match found among all codes with matching computer name / code name pattern: " \
-                    f"{codestrings_by_computer_code}. Possible causes: a) Wrong computer-code combination; b) codes do " \
-                    f"not have a substring specifying either matching queue (partition) or architecture. " \
+                    f"{codestrings_by_computer_code}. Possible causes: a) Wrong computer-code combination; b) codes " \
+                    f"do not have a substring specifying either matching queue (partition) or architecture. " \
                     f"All available codes: {all_codestrings}"
 
         codestring = None
@@ -93,19 +101,18 @@ def get_code(computer_name_pattern: str = "", code_name_pattern: str = "", queue
 
         return codestring, error_msg
 
-    computers = util_computer.get_computers(computer_name_pattern)
+    computers = _jutools_computer.get_computers(computer_name_pattern)
     if not computers:
-        from aiida.common.exceptions import NotExistent
-        raise NotExistent(f"No computer '{computer_name_pattern}' found.")
+        raise _aiida.common.exceptions.NotExistent(f"No computer '{computer_name_pattern}' found.")
     else:
         computer = computers[0]
         if len(computers) > 1:
             print(
-                f"WARNING: For computer name {computer_name_pattern}, found several computers {[c.label for c in computers]}. "
-                f"Will choose first one.")
+                f"WARNING: For computer name {computer_name_pattern}, found several computers "
+                f"{[c.label for c in computers]}. Will choose first one.")
 
     # get cs = codestrings needed for Code.get_from_string(), filter to desired codes on desired computers
-    cs_by_computer_code = [f"{code.label}@{code.computer.label}" for code in Code.objects.all() if
+    cs_by_computer_code = [f"{code.label}@{code.computer.label}" for code in _orm.Code.objects.all() if
                            computer_name_pattern.lower() in code.computer.label.lower()
                            and code_name_pattern.lower() in code.label.lower()]
 
@@ -114,8 +121,8 @@ def get_code(computer_name_pattern: str = "", code_name_pattern: str = "", queue
 
         # first try to get queue_name from computer
         try:
-            queue_name = util_computer.get_least_occupied_queue(computer=computer, gpu=None,
-                                                                with_node_count=False, silent=True)
+            queue_name = _jutools_computer.get_least_occupied_queue(computer=computer, gpu=None,
+                                                                    with_node_count=False, silent=True)
         except NotImplementedError as err:
             # if that failed, only determine by computer_name and code_name_pattern, select first found
             codestring, error_msg = _select_codestring_from_filtered(codestrings_by_computer_code=cs_by_computer_code,
@@ -158,7 +165,7 @@ def get_code(computer_name_pattern: str = "", code_name_pattern: str = "", queue
                           f"Will choose first one. If this is a problem, contact developer.")
 
             queues_harcoded = list(computer_queue_architectures.keys())
-            queues_queried = util_computer.get_queues(computer=computer, gpu=None, with_node_count=False)
+            queues_queried = _jutools_computer.get_queues(computer=computer, gpu=None, with_node_count=False)
             if not set(queues_harcoded) == set(queues_queried):
                 raise ValueError(
                     f"Computer '{computer_name_pattern}' hardcoded queues {queues_harcoded} do not "
@@ -171,10 +178,9 @@ def get_code(computer_name_pattern: str = "", code_name_pattern: str = "", queue
             if not architecture:
                 # since have just that hardcoded queues match actual queues, can conclude
                 # that user has specified non-existant queue
-                import inspect
-                module_name = inspect.getmodulename(inspect.getfile(util_computer.get_queues))
+                module_name = _inspect.getmodulename(_inspect.getfile(_jutools_computer.get_queues))
                 raise KeyError(f"Computer '{computer_name_pattern}' has no queue '{queue_name}'. Use "
-                               f"'{module_name}.{util_computer.get_queues.__name__}()' to get list of queues.")
+                               f"'{module_name}.{_jutools_computer.get_queues.__name__}()' to get list of queues.")
 
             cs_by_computer_code_arch = [cs for cs in cs_by_computer_code if architecture in cs]
 
@@ -186,4 +192,4 @@ def get_code(computer_name_pattern: str = "", code_name_pattern: str = "", queue
             if error_msg:
                 raise ValueError(error_msg)
 
-    return Code.get_from_string(code_string=codestring)
+    return _orm.Code.get_from_string(code_string=codestring)
