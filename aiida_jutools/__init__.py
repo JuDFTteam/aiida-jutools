645955754103e63bec424bf910f922d86d50e16c
johannes wasmer <johannes.wasmer@gmail.com>
2021-09-15 00:24:45 +0200

    __init__.py: correct docstring: 'module' -> 'subpackage'

--- aiida_jutools/__init__.py
+++ aiida_jutools/__init__.py
@@ -19,19 +19,20 @@ you can then call all available tools like so: ``jutools.module.tool()``.
 
 For developers:
 
-- Place larger classes in separate modules, smaller functions in respective ``module/util.py``.
-- Make all available at module level via import in ``module/__init__.py``. See existing files as templates.
-- Also import each new module in the top-level ``__init__.py``.
+- Place larger classes in a subpackage (subfolder) in a separate module (file), smaller functions in the respective
+  subpackage's ``util.py``.
+- Make all available at subpackage level via import in ``subpackage/__init__.py``. See existing files as templates.
+- Also import each new subpackage in the package's top-level ``__init__.py``.
 - Prefix non-user tools with ``_`` to keep user namespace clean and organized.
 - Prefix all imports inside modules with ``_`` to keep user namespace clean and organized. See existing modules
-  for conventions, esp. wrt AiiDA imports.
-- Add ``typing`` hints wherever possible and sensible. See existing modules for examples.
-- When manipulating nodes, implement with 'load or create' pattern: load nodes if already exist, otherwise create.
+  for conventions, e.g. w.r.t. AiiDA imports.
+- Add ``typing`` hints wherever possible and sensible. See existing files for examples.
+- When manipulating AiiDA nodes, implement with 'load or create' pattern: load nodes if already exist, otherwise create.
   Provide a ``dry_run:bool=True`` and verbosity options (``verbosity:int``, ``verbose:bool``, or ``silent:bool``).
 """
 __version__ = "0.1.0-dev1"
 
-# module-level imports
+# Import all subpackages.
 import code
 import computer
 import group
