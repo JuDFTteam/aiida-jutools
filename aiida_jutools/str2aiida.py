e90c6d6110ebc5d418aeee44ff9529e0ae66bb97
johannes wasmer <johannes.wasmer@gmail.com>
2021-09-15 11:00:44 +0200

    Pkg restructure: util_process.py commit2of2: delete

--- aiida_jutools/str2aiida.py
+++ aiida_jutools/str2aiida.py
@@ -16,10 +16,9 @@ Testing script, change '/path/to/cif/file.cif' in last line to existing cif file
 - apart from aiida, pymatgen and PyCifRW packages are needed to use > analyze_symmetry <
 """
 
-import sys
-
 # aiida imports
 from aiida import load_profile
+
 load_profile()
 
 from structure_analyzer import analyze_symmetry
@@ -32,28 +31,31 @@ __license__ = "MIT license, see LICENSE.txt file"
 __version__ = "0.1"
 __contributors__ = u"Roman Kováčik"
 
-def cif2astr(cifpath):
 
+def cif2astr(cifpath):
     prompt = ""
 
     dd = {
-        'fmt' : 'cif',
-        'cifpath' : cifpath,
-        'outmode' : ['a_conv']
+        'fmt': 'cif',
+        'cifpath': cifpath,
+        'outmode': ['a_conv']
     }
 
     structure = analyze_symmetry(dd)
 
     if structure.get('aiida_structure_conventional'):
-
-        print(prompt + structure['aiida_structure_conventional'].extras['check_cif']['message']+'\n')
+        print(prompt + structure['aiida_structure_conventional'].extras['check_cif']['message'] + '\n')
         print(prompt + 'label:           ' + CC1 + structure['aiida_structure_conventional'].label + CEND)
         print(prompt + 'description:     ' + structure['aiida_structure_conventional'].description)
-        print(prompt + 'prototype:       ' + CC2 + structure['aiida_structure_conventional'].extras['prototype']['nprot'] + CEND +
+        print(prompt + 'prototype:       ' + CC2 + structure['aiida_structure_conventional'].extras['prototype'][
+            'nprot'] + CEND +
               ' : ' + CC2 + structure['aiida_structure_conventional'].extras['prototype']['nrw'] + CEND)
-        print(prompt + 'specification:   ' + structure['aiida_structure_conventional'].extras['system']['specification'])
+        print(
+            prompt + 'specification:   ' + structure['aiida_structure_conventional'].extras['system']['specification'])
 
-        print('\n'+'extras:'+'\n')
+        print('\n' + 'extras:' + '\n')
         pprint(structure['aiida_structure_conventional'].extras, width=256)
 
-cif2astr('/path/to/cif/file.cif')
+
+if __name__ == '__main__':
+    cif2astr('/path/to/cif/file.cif')
