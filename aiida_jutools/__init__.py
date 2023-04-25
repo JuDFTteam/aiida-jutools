25b8b95215bb7bff0b935ba13c0b8ecbd98b05da
johannes wasmer <johannes.wasmer@gmail.com>
2023-04-25 13:58:09 +0200

    global import, exclude plugins (kkr etc) ...
    jutools should also be usable without dependency on specific plugins, such as aiida-kkr

--- aiida_jutools/__init__.py
+++ aiida_jutools/__init__.py
@@ -15,6 +15,9 @@
 
 We recommended to use this library with the import statement ``import aiida_jutools as jutools``. In your code,
 you can then call all available tools like so: ``jutools.package.tool()``.
+
+Plugin tools are not loaded globally, as they might not be installed. Instead, import as needed. Example for the
+plugin aiida-kkr: ``import aiida_jutools.plugins.kkr as jutools_kkr``.
 """
 __version__ = "0.1.0-dev1"
 
@@ -26,7 +29,7 @@ from . import io
 from . import logging
 from . import meta
 from . import node
-from . import plugins
+# from . import plugins
 from . import process
 from . import process_functions
 from . import submit
@@ -37,11 +40,10 @@ from . import _dev
 # Potentially problematic imports:
 # - kkr: As soon as aiida-kkr becomes dependent on aiida-jutools, this import MIGHT introduce a circular
 #        dependencies. A simple test (made aiida-kkr import aiida-jutools) had no such effect. But if it
-#        occurs, here a few potential solutions:
-#        - Decouple the kkr package = remove from import list above. Then all code using it must be updated
-#          to import it separately, like from aiida_jutools import kkr as _jutools_kkr. Might break external code.
+#        occurs, here are some possible solutions:
 #        - Hide all aiida-kkr imports = in resp. module, move them inside the tools that use them. If it works,
 #          this might be a solution that does not break external code.
+#        - Remove aiida-kkr dependency altogether from aiida-jutools.
 #
-# The potential problem and the solution stated above, if it becomes one, applies to other JuDFTTeam plugins as well,
+# The potential problem and the solution stated above, if it becomes one, applies to other plugin tools as well,
 # should they start using aiida-jutools as common codebase (aiida-fleur, aiida-spirit, aiida-spex, ...).
